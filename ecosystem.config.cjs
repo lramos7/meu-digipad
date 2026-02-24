b8e4c7a6bc5897968866c87ce32ab7666d94b228
lramos7 <42946453+lramos7@users.noreply.github.com>
2026-02-24 12:42:46 -0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,14 @@
 module.exports = {
-	apps: [{
-		name: 'Digipad',
-		script: './server/index.js',
-		node_args: [
-			'--expose-gc'
-		],
-		autorestart: true,
-		max_restarts: 10,
-		env: {
-			'NODE_ENV': 'development'
-		},
-		env_production: {
-			'NODE_ENV': 'production'
-		}
-	}]
+  apps: [{
+    name: 'Digipad',
+    script: './dist/server/entry.mjs',
+    node_args: [
+      '--expose-gc'
+    ],
+    autorestart: true,
+    max_restarts: 10,
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
 }
